4fdae6873ac39cc47d23cf479ac4305575e76eec
err-arman <err.arman@gmail.com>
2025-02-09 17:56:00 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        fontSize: {
+            sm: ['14px', '20px'],
+            base: ['16px', '24px'],
+            lg: ['20px', '28px'],
+            xl: ['24px', '32px'],
+          },
+      extend: {},
+    },
+    plugins: [],
+  }
